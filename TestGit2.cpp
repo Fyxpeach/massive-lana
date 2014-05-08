d817feb2b6fedc8fa37e90cc41c59f54532ea00d
Fyx <fyx@summerdeMac-mini.local>
2014-05-08 10:14:52 +0800

    branch2 add "this is branch2"

---
+++ TestGit2.cpp
@@ -0,0 +1,9 @@
+//
+//  TestGit2.cpp
+//  TestGit
+//
+//  Created by Fyx on 14-5-8.
+//
+//
+
+#include "TestGit2.h"
---
+++ TestGit2.h
@@ -0,0 +1,14 @@
+//
+//  TestGit2.h
+//  TestGit
+//
+//  Created by Fyx on 14-5-8.
+//
+//  this is branch2
+
+#ifndef __TestGit__TestGit2__
+#define __TestGit__TestGit2__
+
+#include <iostream>
+
+#endif /* defined(__TestGit__TestGit2__) */
